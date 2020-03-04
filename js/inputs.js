8ae66b1a305cef74d7e44c94b85832a8e27bd7c5
Rob Johnson <robjohnson@MacBook-Pro.local>
2020-03-04 14:01:31 +0000

    add js

--- js/inputs.js
+++ js/inputs.js
@@ -1,14 +1,9 @@
 "use-strict";
-// use-strict catches code errors before they cause damage
 
-/*
-  the containing IIFE (Immediately-invoked Function Expression)
-  keeps your variables out of the global namespace
-*/
 
 (function () {
 
-  // DATE =======================================
+  
   const showDate = () => {
     let d = new Date(datePicker.value);
     theDate.innerText = d.toDateString();
@@ -19,7 +14,7 @@
   theDate.innerHTML = today.toDateString();
 
 
-  // COLOUR =====================================
+
   const showColour = () => {
     let c = theColour.value;
     colourBox.style.backgroundColor = c;
@@ -29,7 +24,7 @@
   showColour();
 
 
-  // RANGE ======================================
+
   const showRange = () => {
     let r = theRange.value;
   	// note: the '%' unit is essential
